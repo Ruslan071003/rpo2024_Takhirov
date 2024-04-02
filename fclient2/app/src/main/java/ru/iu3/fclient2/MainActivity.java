3f1d2267ef30bb18daaaef823ae9135d939c4503
Ruslan071003 <ruslan.takhirov07@yandex.ru>
2024-04-02 13:38:02 +0300

    fourth lab done

--- fclient2/app/src/main/java/ru/iu3/fclient2/MainActivity.java
+++ fclient2/app/src/main/java/ru/iu3/fclient2/MainActivity.java
@@ -7,14 +7,19 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.IOUtils;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import ru.iu3.fclient2.databinding.ActivityMainBinding;
@@ -116,8 +121,9 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
 
     public void onButtonClick(View v)
     {
-        byte[] trd = stringToHex("9F0206000000000100");
-        boolean ok = transaction(trd);
+//        byte[] trd = stringToHex("9F0206000000000100");
+//        boolean ok = transaction(trd);
+
 //        new Thread(()-> {
 //            try {
 //                byte[] trd = stringToHex("9F0206000000000100");
@@ -129,10 +135,44 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
 //            } catch (Exception ex) {
 //                // todo: log error
 //            }
-//        }).start();
-        Intent it = new Intent(this, PinPadActivity.class);
-//        startActivity(it);
-        activityResultLauncher.launch(it);
+////        }).start();
+
+//        Intent it = new Intent(this, PinPadActivity.class);
+////        startActivity(it);
+//        activityResultLauncher.launch(it);
+        testHttpClient();
+    }
+
+    protected void testHttpClient()
+    {
+        new Thread(() -> {
+            try {
+                HttpURLConnection uc = (HttpURLConnection)
+                        (new URL("https://10.0.2.2:8081/api/v1/title").openConnection());
+                InputStream inputStream = uc.getInputStream();
+                String html = IOUtils.toString(inputStream);
+                String title = getPageTitle(html);
+                runOnUiThread(() ->
+                {
+                    Toast.makeText(this, title, Toast.LENGTH_LONG).show();
+                });
+
+            } catch (Exception ex) {
+                Log.e("fapptag", "Http client fails", ex);
+            }
+        }).start();
+    }
+    protected String getPageTitle(String html)
+    {
+        int pos = html.indexOf("<title");
+        String p="not found";
+        if (pos >= 0)
+        {
+            int pos2 = html.indexOf("<", pos + 1);
+            if (pos >= 0)
+                p = html.substring(pos + 7, pos2);
+        }
+        return p;
     }
 
     public native void log(String value);
